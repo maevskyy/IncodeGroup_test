9d5f4199c5f0023c83029655d41513b93036f07d
maevskyy <maevskijdimitrij1@gmail.com>
2024-01-07 14:01:45 +0200

    destructuring of Columns component

--- client/src/widjets/main/columns/index.tsx
+++ client/src/widjets/main/columns/index.tsx
@@ -3,62 +3,29 @@ import React, { useState } from 'react';
 import { defaultColums } from './entities/default.data';
 import Column from './components/column';
 import AddColumn from './components/column/AddColumn';
-import { IColumn, ITask } from './entities/types';
+import { IColumn } from './entities/types';
+import { stateControler } from './entities/stateControler';
+import { useDranNDrop } from './entities/useDragNDrop';
 
-const Columns = () => {
+const Columns: React.FC = () => {
    const [allColumns, setAllColumns] = useState<IColumn[]>(defaultColums);
-
-   const addNewColumnHandler = (newColumn: IColumn) =>
-      setAllColumns((prevColumns) => [...prevColumns, newColumn]);
-
-   const deleteColumnHanlder = (columnId: string) =>
-      setAllColumns((prevColumns) =>
-         prevColumns.filter((column) => column.id !== columnId),
-      );
-
-   const addNewTaskHandler = (columnId: string, task: ITask) => {
-      setAllColumns((prevColumns) =>
-         prevColumns.map((column) => {
-            if (column.id === columnId) {
-               return {
-                  ...column,
-                  tasks: [...column.tasks, task],
-               };
-            }
-            return column;
-         }),
-      );
-   };
-
-   const deleteTaskHandler = (columnId: string, taskId: string) => {
-      setAllColumns((prevColumns) =>
-         prevColumns.map((column) => {
-            if (column.id === columnId) {
-               return {
-                  ...column,
-                  tasks: column.tasks.filter((task) => task.id !== taskId),
-               };
-            }
-            return column;
-         }),
-      );
-   };
+   // add\delete column and add\delete task
+   const stateControllers = stateControler(setAllColumns);
+   // dragLeave, dragOver and so on
+   const dragAndDrop = useDranNDrop(setAllColumns);
 
    return (
       <>
          {allColumns.map((column) => (
-            <Column
-               key={column.id}
-               id={column.id}
-               title={column.title}
-               Icon={column.Icon}
-               tasks={column.tasks}
-               deleteColumnHandler={deleteColumnHanlder}
-               addNewTaskHandler={addNewTaskHandler}
-               deleteTaskHandler={deleteTaskHandler}
-            />
+            <div key={column.id}>
+               <Column
+                  columnFullInfo={column}
+                  stateControllers={stateControllers}
+                  dragAndDrop={dragAndDrop}
+               />
+            </div>
          ))}
-         <AddColumn addColumn={addNewColumnHandler} />
+         <AddColumn addColumn={stateControllers.addNewColumnHandler} />
       </>
    );
 };
